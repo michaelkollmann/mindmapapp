7ae271a53c297d386a454eb52fe6f4994da9104f
Michael Kollmann <me@michaelkollmann.de>
2012-06-26 17:05:25 +0200

    first commit

---
+++ mindmapapp/Model/MNode.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI;
+
+namespace mindmapapp.Model
+{
+    public class MNode : ObservableCollection<MNode>
+    {
+        private string _title;
+        private Color _color;
+        private Point _point;
+
+
+
+        public MNode(string Title, Color Color, Point Point)
+        {
+            this.Title = Title;
+            this.Color = Color;
+            this.Point = Point;
+        }
+
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+        public Color Color
+        {
+            get { return _color; }
+            set { _color = value; }
+        }
+        public Point Point
+        {
+            get { return _point; }
+            set { _point = value; }
+        }
+
+        
+    }
+}
---
+++ mindmapapp/ViewModel/VMNode.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using mindmapapp.Model;
+
+namespace mindmapapp.ViewModel
+{
+    public class VMNode : ICollection<VMNode>, INotifyCollectionChanged, INotifyPropertyChanged
+    {
+        private MNode _model;
+        private readonly ICollection<VMNode> _collection = new Collection<VMNode>();
+
+        #region Constructors
+        public VMNode(MNode Model)
+            : this(Model, Enumerable.Empty<MNode>()) { }
+        public VMNode(MNode Model, IEnumerable<MNode> Children)
+        {
+            _model = Model;
+            foreach (MNode item in Children)
+            {
+                Add(new VMNode(item));
+            }
+        }
+        #endregion
+
+        #region ICollection<VMNode>
+        public void Add(VMNode item)
+        {
+            _collection.Add(item);
+            RaiseCollectionChanged(NotifyCollectionChangedAction.Add);
+        }
+
+        public void Clear()
+        {
+            _collection.Clear();
+            RaiseCollectionChanged(NotifyCollectionChangedAction.Reset);
+        }
+
+        public bool Contains(VMNode item)
+        {
+            return _collection.Contains(item);
+        }
+
+        public void CopyTo(VMNode[] array, int arrayIndex)
+        {
+            _collection.CopyTo(array, arrayIndex);
+        }
+
+        public int Count
+        {
+            get { return _collection.Count; }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return _collection.IsReadOnly; }
+        }
+
+        public bool Remove(VMNode item)
+        {
+            RaiseCollectionChanged(NotifyCollectionChangedAction.Remove);
+            return _collection.Remove(item);
+        }
+
+        public IEnumerator<VMNode> GetEnumerator()
+        {
+            return _collection.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+        #endregion
+
+        #region INotifyCollectionChanged
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+
+        private void RaiseCollectionChanged(NotifyCollectionChangedAction Action)
+        {
+            if (CollectionChanged != null)
+            {
+                CollectionChanged(this, new NotifyCollectionChangedEventArgs(Action));
+            }
+        }
+        #endregion
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+
+        #region Methods
+        public void AddEnumerable(IEnumerable<VMNode> Nodes)
+        {
+            foreach (VMNode item in Nodes)
+            {
+                Add(item);
+            }
+        }
+        #endregion
+    }
+}
